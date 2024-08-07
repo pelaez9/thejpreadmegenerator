79e3ca48bce5794cbf5733bb111bbb5d7fd88fde
pelaez9 <pelaez9@hotmail.com>
2024-08-07 13:30:37 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# thejpreadmegenerator
-This is the code for a readme generator.
+
+# ooooooo
+
+## Description
+sdmn,zvmxvnx,mvn
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+dsdfvsdfgsdfvsdfgsd
+
+## Usage
+dsfgsdfsdfgfsgsd
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+dsfbsfdbssdfbsdfbs
+
+## Tests
+zxcvvaxcvzxcv<cxv
+
+## Questions
+For any questions, please contact me with the information below:
+
+GitHub: [pelaez9](https://github.com/pelaez9)
+Email: pelaez9@hotmail.com
+  
---
+++ index.js
@@ -0,0 +1,102 @@
+import fs from 'fs';
+import inquirer from 'inquirer';
+
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Enter a description of your project:',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Enter installation instructions:',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Enter usage information:',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Enter contribution guidelines:',
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: 'Enter test instructions:',
+    name: 'tests',
+  },
+  {
+    type: 'list',
+    message: 'Choose a license for your project:',
+    name: 'license',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    message: 'Enter your GitHub username:',
+    name: 'github',
+  },
+  {
+    type: 'input',
+    message: 'Enter your email address:',
+    name: 'email',
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  const readmeContent = generateReadme(answers);
+  fs.writeFile('./output/README.md', readmeContent, (err) => {
+    if (err) {
+      console.error('Error', err);
+    } else {
+      console.log('README.md generated');
+    }
+  });
+});
+
+// Create function for the badge https://img.shields.io/badge/any_text-you_like-blue
+
+function generateReadme(answers) {
+  return `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For any questions, please contact me with the information below:
+
+GitHub: [${answers.github}](https://github.com/${answers.github})
+Email: ${answers.email}
+  `;
+}
